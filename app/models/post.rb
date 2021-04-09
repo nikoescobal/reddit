d88618b94a516119d798729d82df190debb5e338
Niko Escobal <niko.escobal@gmail.com>
2021-04-09 18:45:30 +0800

    Updated files

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
   validates :title, :body, :user_id, presence: true
   validates_uniqueness_of :title
-
 end
