e7f3b87c1d1cec78f3ea719ea6e37686005531c0
Erez <erezfree29@gmail.com>
2021-04-09 11:00:24 -0700

    add validations

---
+++ db/migrate/20210409173745_add_title_and_body_to_comment.rb
@@ -0,0 +1,6 @@
+class AddTitleAndBodyToComment < ActiveRecord::Migration[6.1]
+  def change
+    add_column :comments, :title, :string
+    add_column :comments, :body, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_09_170047) do
+ActiveRecord::Schema.define(version: 2021_04_09_173745) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
+    t.text "body"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
